08ccd19c9c0740bed65dac4865ddaf1286a9b979
PrestaEdit <j.danse@prestaedit.com>
2014-04-26 20:17:22 +0200

    // 2014 & 3.0, ;)

--- navmegadrownevo.php
+++ navmegadrownevo.php
@@ -5,8 +5,8 @@
  * @category   	Module / front_office_features
  * @author     	PrestaEdit <j.danse@prestaedit.com> (since 2.0)
  * @author     	DevForEver (special thanks to him)
- * @copyright  	2013 PrestaEdit
- * @version   	2.0
+ * @copyright  	2014 PrestaEdit
+ * @version   	3.0
  * @link       	http://www.prestaedit.com/
  * @since      	File available since Release 1.0
 */
