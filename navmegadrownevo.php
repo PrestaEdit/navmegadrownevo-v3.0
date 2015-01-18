d2e82aed5f8dee31e87aec66efc0325fc5a1a9cd
PrestaEdit <j.danse@prestaedit.com>
2015-01-18 22:55:15 +0100

    //license

--- navmegadrownevo.php
+++ navmegadrownevo.php
@@ -1,14 +1,34 @@
 <?php
 /**
- * Module MeGa DrOwN mEnU Evolution - Main file
- *
- * @category   	Module / front_office_features
- * @author     	PrestaEdit <j.danse@prestaedit.com> (since 2.0)
- * @author     	DevForEver (special thanks to him)
- * @copyright  	2015 PrestaEdit
- * @version   	3.0
- * @link       	http://www.prestaedit.com/
- * @since      	File available since Release 1.0
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2015 PrestaShop SA
+*  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  @category  Module / front_office_features
+*  @author    PrestaEdit <j.danse@prestaedit.com> (since 2.0)
+*  @author    DevForEver (special thanks to him)
+*  @copyright 2015 PrestaEdit
+*  @version   3.0
+*  @link      http://www.prestaedit.com/
+*  @since     File available since Release 1.0
+*  International Registered Trademark & Property of PrestaShop SA
 */
 
 // Security
--- sql/uninstall.php
+++ sql/uninstall.php
@@ -1,13 +1,1 @@
-<?php
-
-	// Init
-	$sql = array();
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button`;';			
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_organization`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_lang`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_langcat`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_link_cat`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_link`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_parameters`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_custom_menu`';
-	$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_custom_menu_lang`';
+<?php/*** 2007-2015 PrestaShop** NOTICE OF LICENSE** This source file is subject to the Open Software License (OSL 3.0)* that is bundled with this package in the file LICENSE.txt.* It is also available through the world-wide-web at this URL:* http://opensource.org/licenses/osl-3.0.php* If you did not receive a copy of the license and are unable to* obtain it through the world-wide-web, please send an email* to license@prestashop.com so we can send you a copy immediately.** DISCLAIMER** Do not edit or add to this file if you wish to upgrade PrestaShop to newer* versions in the future. If you wish to customize PrestaShop for your* needs please refer to http://www.prestashop.com for more information.**  @author    PrestaShop SA <contact@prestashop.com>*  @copyright 2007-2015 PrestaShop SA*  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)*  International Registered Trademark & Property of PrestaShop SA*/$sql = array();$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button`;';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_organization`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_lang`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_langcat`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_link_cat`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_button_link`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_parameters`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_custom_menu`';$sql[] = 'DROP TABLE IF EXISTS `'._DB_PREFIX_.'admevo_custom_menu_lang`';
